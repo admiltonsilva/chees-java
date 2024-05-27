26a14df3e70f68acf48fef21dd8cb87c99b169c0
Admilton Silva <admilton_silva@hotmail.com>
2024-05-27 11:09:44 -0300

    testando

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,12 +1,10 @@
 
 package chess;
-
-import javax.swing.JOptionPane;
-
 public class Chess {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("ok");
         System.out.println("ok");
     }
     
