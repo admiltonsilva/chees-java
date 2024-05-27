3fa0eca3b3b0a2fb50f9db3d508bfeebd5868d48
Admilton Silva <admilton_silva@hotmail.com>
2024-05-27 11:04:37 -0300

    Project created

---
+++ src/chess/Chess.java
@@ -0,0 +1,13 @@
+
+package chess;
+
+import javax.swing.JOptionPane;
+
+public class Chess {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("ok");
+    }
+    
+}
